f34fe5ad88406b2e56a27b75464e99f95f79191b
pvinod <kvp5620@gmail.com>
2023-11-09 11:31:56 +0530

    vinod changes the code

--- src/com/flipkart/Flipcart.java
+++ src/com/flipkart/Flipcart.java
@@ -5,6 +5,12 @@ public class Flipcart {
 	public void syso() {
 		
 		System.out.println("welcome to java");
+		
+		System.out.println("new added code");
+		
+		System.out.println("newly changed the code");
+		
+		System.out.println("new codes changed by vinod");
 
 	}
 
